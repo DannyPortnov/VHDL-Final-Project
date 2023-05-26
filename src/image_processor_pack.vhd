cd456d41786430f0d7ef46561f55c233cd0d0a8e
danih <danih@DESKTOP-GCQI5FM>
2023-05-26 16:38:45 +0300

    Fixed compilation errors in packge: - Added ':' in constants declaration - Corrected convert_to_eight_bit return value's type - Added library import - Added function signatures to my functions - Fixed clr_conv assignment operator

--- src/image_processor_pack.vhd
+++ src/image_processor_pack.vhd
@@ -1,30 +1,39 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 package image_processor_pack is
-    constant G_PIXELS_NUM     integer := 64;
-    constant R_B_PIXELS_NUM   integer := 32;
-    constant R_B_CONV_PARAM   real    := 8.225806452;
-    constant G_CONV_PARAM     real    := 4.047619048;
+    constant G_PIXELS_NUM   : integer := 64;
+    constant R_B_PIXELS_NUM :  integer := 32;
+    constant R_B_CONV_PARAM :  real    := 8.225806452;
+    constant G_CONV_PARAM   :  real    := 4.047619048;
     -- function that converts color from L<=8 bit to 8 bit representation
-    function convert_to_eight_bit (clr_data : integer range 0 to G_PIXELS_NUM)    return std_logic_vector(7 downto 0);
-
-
+    -- To Niv: the type of a function’s arguments and return value
+    -- must be specified using a type mark (std_logic_vector), not a subtype indication (std_logic_vector(7 downto 0)). 
+    function convert_to_eight_bit (clr_data : integer range 0 to G_PIXELS_NUM) return std_logic_vector;
+    -- function that converts BCD to 7 segment representation
+    function bcd_to_7seg (BCD_IN: integer range 0 to 9) return std_logic_vector;
+    -- function that divides two integers
+    function divide(dividend: integer; divisor: integer) return integer;
+    -- function that returns nth digit of a number, starting from 1
+    function get_nth_digit(num: integer; n: integer) return integer;
 end package;
 
 
 package body image_processor_pack is   
-    function convert_to_eight_bit (clr_data : integer range 0 to G_PIXELS_NUM)    return std_logic_vector(7 downto 0) is
+    
+    function convert_to_eight_bit (clr_data : integer range 0 to G_PIXELS_NUM)    return std_logic_vector is
         
         variable clr_conv : std_logic_vector(7 downto 0);
 
     begin
+        -- To Niv: variables are assigned values using the variable assignment operator :=, not the signal assignment operator <=.
         if (clr_data < R_B_PIXELS_NUM) and (clr_data >= 0) then
-            clr_conv <= std_logic_vector(to_unsigned(clr_data * integer(floor(real(255)/real(R_B_CONV_PARAM))) , clr_conv'length)); 
+            clr_conv := std_logic_vector(to_unsigned(clr_data * integer(floor(real(255)/real(R_B_CONV_PARAM))) , clr_conv'length)); 
         
         elsif (clr_data >= R_B_PIXELS_NUM) and (clr_data < G_PIXELS_NUM) then
-            clr_conv <= std_logic_vector(to_unsigned(clr_data * integer(floor(real(255)/real(G_CONV_PARAM))) , clr_conv'length)); 
+            clr_conv := std_logic_vector(to_unsigned(clr_data * integer(floor(real(255)/real(G_CONV_PARAM))) , clr_conv'length)); 
         end if;
         return (clr_conv);
     end convert_to_eight_bit;
