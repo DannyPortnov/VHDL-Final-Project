0a3c40d8d47cb12056e1d0ea0aaea720a100e8c3
nivkopo <nivko8746@gmail.com>
2023-05-27 15:37:59 +0300

    finished timing generator + test bench

--- src/timing_generator.vhd
+++ src/timing_generator.vhd
@@ -1,23 +1,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use WORK.image_processor_pack.all;
 
 entity timing_generator is
     generic (
-        G_RESET_ACTIVE_VALUE        : std_logic := 0;
-        G_PIXELS_PER_LINE           : integer := 800;
-        G_PIXELS_PER_FRAME          : integer := 525;
-        -- VISIBLE_PIXELS_PER_LINE     : integer := 640;
-        -- VISIBLE_PIXELS_PER_FRAME    : integer := 480;
+        G_RESET_ACTIVE_VALUE        : std_logic := '0'
     );
     port (
         CLK         : in  std_logic;
         RST         : in  std_logic;
-        H_CNT       : out integer range 0 to PIXELS_PER_LINE-1;
-        V_CNT       : out integer range 0 to PIXELS_PER_FRAME-1;
-        H_SYNC      : out std_logic;
-        V_SYNC      : out std_logic;
-        VS          : out std_logic
+        H_CNT       : out integer range 0 to C_PIXELS_PER_LINE-1  := 0;
+        V_CNT       : out integer range 0 to C_PIXELS_PER_FRAME-1 := 0;
+        H_SYNC      : out std_logic := '1';
+        V_SYNC      : out std_logic := '1';
+        VS          : out std_logic := '0'
     );
 end entity;
 
@@ -34,16 +31,13 @@ architecture behave of timing_generator is
     constant VS_pulse       : integer:= 2;   -- vsync retrace
 
 
-    signal h_cnt_sig		: integer range 0 to G_PIXELS_PER_LINE - 1 	:= 0;	-- This signal counts how much pixles are passed on each line, every 800 pixles cnt_H_pixels = 0.
-	signal v_cnt_sig		: integer range 0 to G_PIXELS_PER_FRAME - 1	:= 0; 	-- This signal counts how much lines are passed in pixles units.
+    signal h_cnt_sig		: integer range 0 to C_PIXELS_PER_LINE - 1 	:= 0;	-- This signal counts how much pixles are passed on each line, every 800 pixles cnt_H_pixels = 0.
+	signal v_cnt_sig		: integer range 0 to C_PIXELS_PER_FRAME - 1	:= 0; 	-- This signal counts how much lines are passed in pixles units.
 
 
     begin
     process(CLK,RST)
     begin
-        H_CNT <= h_cnt_sig;
-        V_CNT <= v_cnt_sig;
-
         if RST = G_RESET_ACTIVE_VALUE then   --reset output 
             h_cnt_sig <= 0;
             v_cnt_sig <= 0;
@@ -53,45 +47,58 @@ architecture behave of timing_generator is
 
         elsif rising_edge(CLK) then
         -- handle horizontal and vertical counters
-            if (h_cnt_sig < G_PIXELS_PER_LINE - 1) then
+            if (h_cnt_sig < C_PIXELS_PER_LINE - 1) then
                 h_cnt_sig <= h_cnt_sig + 1;
-            elsif (h_cnt_sig = G_PIXELS_PER_LINE - 1) then
+            elsif (h_cnt_sig = C_PIXELS_PER_LINE - 1) then
                 h_cnt_sig <= 0;
-                if (v_cnt_sig < G_PIXELS_PER_FRAME - 1) then
-
-                    -- need to check if VS is active high just for 1 clk pulse!
-                    if (v_cnt_sig = (V_visible + V_FP)) then
-                        VS <= '1';
-                    else
-                        VS <= '0';
-                    end if;    
-                    
-                    v_cnt_sig <= v_cnt_sig + 1;
-                
-                elsif (h_cnt_sig = G_PIXELS_PER_FRAME - 1) then
+                if (v_cnt_sig < C_PIXELS_PER_FRAME - 1) then    
+                    v_cnt_sig <= v_cnt_sig + 1;                  
+                elsif (v_cnt_sig = C_PIXELS_PER_FRAME - 1) then
                     v_cnt_sig <= 0;
                 end if;
             end if;
 
+        -- vertical sync for 480 pixels + update VS
+            if ((v_cnt_sig = (V_visible + V_FP - 1)) and (h_cnt_sig = (C_PIXELS_PER_LINE - 1))) then
+                VS <= '1';  -- update VS- active high for only 1 clk pulse!
+                V_SYNC <= '0'; 
+            elsif ((v_cnt_sig = (V_visible + V_FP + VS_pulse - 1)) and (h_cnt_sig = (C_PIXELS_PER_LINE - 1))) then
+                V_SYNC <= '1';
+            else
+                VS <= '0';
+            end if; 
+
         -- horizontal sync for 640 pixels
-            if (h_cnt_sig >= (H_visible + H_FP) and h_cnt_sig < (G_PIXELS_PER_LINE - H_BP)) then
+            if (h_cnt_sig >= (H_visible + H_FP - 1) and h_cnt_sig < (C_PIXELS_PER_LINE - H_BP - 1)) then
                 H_SYNC <= '0';
-            elsif (h_cnt_sig < (H_visible + H_FP) or h_cnt_sig >= (G_PIXELS_PER_LINE - H_BP)) then
+            elsif (h_cnt_sig < (H_visible + H_FP - 1) or h_cnt_sig >= (C_PIXELS_PER_LINE - H_BP - 1)) then
                 H_SYNC <= '1';
-            -- elsif (h_cnt_sig = G_PIXELS_PER_LINE - 1) then
+            -- elsif (h_cnt_sig = C_PIXELS_PER_LINE - 1) then
             --     H_SYNC <= '1';                
             end if;
 
+    --*********************************************************************************************************
+    -- old implementation for vs + vertical sync (with delay of 1 clk for V_SYNC)
+    --*********************************************************************************************************
+        -- update VS
+            -- if ((v_cnt_sig = (V_visible + V_FP - 1)) and (h_cnt_sig = (C_PIXELS_PER_LINE - 1))) then
+            --     VS <= '1';  -- update VS- active high for only 1 clk pulse!
+            -- else
+            --     VS <= '0';
+            -- end if; 
         -- vertical sync for 480 pixels
-            if (v_cnt_sig >= (V_visible + V_FP) and v_cnt_sig < (G_PIXELS_PER_FRAME - V_BP)) then
-                V_SYNC <= '0';
-            elsif (v_cnt_sig < (V_visible + V_FP) or v_cnt_sig >= (G_PIXELS_PER_FRAME - V_BP)) then
-                V_SYNC <= '1';
-            -- elsif (v_cnt_sig = G_PIXELS_PER_LINE - 1) then
+            -- if (v_cnt_sig > (V_visible + V_FP - 1) and v_cnt_sig <= (C_PIXELS_PER_FRAME - V_BP - 1)) then
+            --     V_SYNC <= '0';
+            -- elsif (v_cnt_sig <= (V_visible + V_FP - 1) or v_cnt_sig > (C_PIXELS_PER_FRAME - V_BP - 1)) then
             --     V_SYNC <= '1';
-            end if;
+            -- elsif (v_cnt_sig = C_PIXELS_PER_LINE - 1) then
+            --     V_SYNC <= '1';
+            -- end if;
+    --*********************************************************************************************************
         end if;
     end process;
+    H_CNT <= h_cnt_sig;
+    V_CNT <= v_cnt_sig;
 end architecture;
 
 
