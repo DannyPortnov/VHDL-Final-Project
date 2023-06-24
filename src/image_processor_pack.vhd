a05c7643e1e23cddfb24cfd9101a185e368a8cf4
nivkopo <nivko8746@gmail.com>
2023-06-24 19:34:38 +0300

    fixed comments

--- src/image_processor_pack.vhd
+++ src/image_processor_pack.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.math_real.all;
 
 package image_processor_pack is
     constant C_PIXELS_PER_LINE		  : integer := 800;
